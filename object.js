146b42b3faa09f40c65d0ee28da28ac825822590
ziggy <47399462+anabtrzz@users.noreply.github.com>
2020-10-29 00:08:22 -0300

    Create object.js

---
+++ object.js
@@ -0,0 +1,24 @@
+//OBJECT estrutura
+
+var obj = {
+    característica 1,
+    característica 2,
+    característica 3,
+};
+console.log(obj);
+
+
+
+//exemplos
+
+var me = {
+name:zgy,
+age:21,
+} 
+//obj só com infos
+
+var obj = {
+    start:function name(params) {
+    }
+} 
+//objeto com uma propriedade que é uma função
